14264623388c4c5e37776abb87554712e5630a6f
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-10-15 21:44:35 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,23 @@
 # don't forget to add: require 'pry'
-
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
+  if (string[0] == "s")
+    count = 0
+    while count < 10
+      string = "s" + string
+      count = count + 1
+      string
+    end
   else
     string
   end
+  string
 end
+
+# require 'pry'
+# #def snake_it_up(string)
+#   if string[0] == "s"
+#     10 * "s" + string
+#   else
+#     string
+#   end
+# end
