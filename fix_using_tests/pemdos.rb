63d29f3161b610a7401cd61c38f34b9ea0dda87f
Jakob Persons <jakobep24@gmail.com>
2020-10-16 16:04:18 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,13 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    "s" * 10 + string #"s" is multiplied 10 times before adding the string argument --> pemdos
+    # binding.pry
   else
     string
   end
 end
+
+snake_it_up("surprise!")
--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,27 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
+def state_log(star_date) #define method state_log with argument of date
+  puts "Captain's Log, star date #{star_date}." #puts Captain's Log with star date
+end
 
-def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+crew = ["Jake", "Brynn", "Chase", "Brittany", "Eric"] #set crew equal to array of names
+
+def greet_crew(crew) #us array of names as argument for greet_crew method
+  crew.each do |crew_member| #iterate over crew array and
+    puts "Hello #{crew_member}." #puts greeting to each crew member
+  end
 end
 
 def engage
-  puts state_log(date)
-  date = generate_star_date
+  crew = ["Jake", "Brynn", "Chase", "Brittany", "Eric"] #define crew variable equal to array of names
+  date = generate_star_date #set generate_star_date method equal to variable date
+  state_log(date) #pass date to state_log method as argument
+  greet_crew(crew) #call greet_crew method with crew variable passed as argument
 end
+
+engage
